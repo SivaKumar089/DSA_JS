4c627fe64d803ac1a6b1913d81ab44b5c7a9648d
Sivakumar089 <kumarsiva200426@gmail.com>
2025-09-25 20:49:20 +0530

    today dsa learn

---
+++ day-17/climbStairsRec.js
@@ -0,0 +1,7 @@
+function climbStairsRec(n) {
+  if (n === 0) return 1;
+  if (n === 1) return 1;
+  return climbStairsRec(n - 1) + climbStairsRec(n - 2);
+}
+
+console.log(climbStairsRec(5)); // Output: 8
